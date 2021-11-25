b376112fe7c5ed97754f77645f39f4eb30c64747
SugarF0x <sugarfox.dev@gmail.com>
2021-11-25 20:51:30 +0300

    feat(footer): adjust footer content

--- components/Home/Footer.vue
+++ components/Home/Footer.vue
@@ -1,62 +1,19 @@
 <template>
   <footer id="footer">
-    <v-row no-gutters>
-      <v-col cols="12" sm="4">
-        <Card color="primary" icon="phone">
-          <template #title>Lorem ipsum</template>
-          <template #text>
-            <ul class="list">
-              <li v-for="phone in PHONES" :key="phone">
-                <a :href="`tel:${phone}`">{{ formatPhone(phone) }}</a>
-              </li>
-            </ul>
-          </template>
-        </Card>
-      </v-col>
-      <v-col cols="12" sm="4">
-        <Card color="secondary" icon="email">
-          <template #title>Lorem ipsum</template>
-          <template #text>
-            <ul class="list">
-              <li v-for="mail in MAILS" :key="mail">
-                <a :href="`mailto:${mail}`">{{ mail }}</a>
-              </li>
-            </ul>
-          </template>
-        </Card>
-      </v-col>
-      <v-col cols="12" sm="4">
-        <Card color="primary" icon="home-map-marker">
-          <template #title>Lorem ipsum</template>
-          <template #text>
-            Lorem ipsum dolor
-            <br />
-            Lorem ipsum dolor sit
-          </template>
-        </Card>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row justify="center">
+        <v-col class="display-1" cols="12" md="8">
+          Сайт подготовлен 5&#8209;й командой в рамках итоговой презентации проекта "Артократия"</v-col
+        >
+      </v-row>
+    </v-container>
   </footer>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "@nuxtjs/composition-api"
-import { MAILS, PHONES } from "~/assets/consts"
-import { formatPhone } from "~/assets/util"
-
-export default defineComponent({
-  setup() {
-    return {
-      MAILS,
-      PHONES,
-      formatPhone,
-    }
-  },
-})
-</script>
-
-<style lang="sass" scoped>
-.list
-  list-style: none
-  padding: 0
+<style lang="scss">
+footer {
+  background-color: black;
+  color: white;
+  text-align: center;
+}
 </style>
