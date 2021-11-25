5d22a8e9eeaa08b7aacd820b9e7ac77889f81a22
SugarF0x <sugarfox.dev@gmail.com>
2021-11-25 22:46:51 +0300

    feat(img): replace placeholder images with our assets

--- components/Home/Recent.vue
+++ components/Home/Recent.vue
@@ -6,8 +6,7 @@
     </v-container>
     <v-row no-gutters>
       <v-col v-for="(image, n) in IMAGES" :key="`recentImage${n}`" cols="12" sm="6" md="3">
-        <!--        <pre-image :src="image" aspect-ratio="1.5" />-->
-        <v-img :src="image" />
+        <pre-image :src="image" aspect-ratio="1.5" />
       </v-col>
     </v-row>
   </section>
@@ -16,12 +15,7 @@
 <script lang="ts">
 import { defineComponent } from "@nuxtjs/composition-api"
 
-const IMAGES = [
-  `https://picsum.photos/id/527/400`,
-  `https://picsum.photos/id/683/400`,
-  `https://picsum.photos/id/206/400`,
-  `https://picsum.photos/id/545/400`,
-]
+const IMAGES = [`recent/1.jpg`, `recent/2.jpg`, `recent/3.jpg`, `recent/4.jpg`]
 
 export default defineComponent({
   setup() {
